43fcba866e4bb3c68cea1b8411c0123a3213ccef
Mystic-Pudding <totocaca@naver.com>
2022-01-10 20:23:42 +0900

    .

--- Flutter/network/README.md
+++ Flutter/network/README.md
@@ -1,4 +1,4 @@
-# network
+# networkpython
 
 A new Flutter project.
 
--- Flutter/network/android/app/src/main/kotlin/com/example/network/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.network
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
