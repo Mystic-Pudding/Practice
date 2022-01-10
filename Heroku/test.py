bc9de1623d7002c60134ed001a5887edb5ffbdbe
Mystic-Pudding <totocaca@naver.com>
2022-01-10 20:27:16 +0900

    .

--- Heroku/test.py
+++ Heroku/test.py
@@ -14,16 +14,13 @@
 # for i in range(3):
 #     predict_array.append(int(predict[i][0]))
 from flask import Flask, json, jsonify, request
-from werkzeug.serving import WSGIRequestHandler
-
 app = Flask(__name__)
 
 @app.route('/',methods = ['GET'])
 def hello_world():
-    return jsonify({'number':'numbers'}),200
+    return jsonify({'number':'numbers'})
 
 if __name__ == '__main__':
-    WSGIRequestHandler.protocol_version = "HTTP/1.1"
     app.run(debug=True)
 
     
