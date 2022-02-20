cb0921d8f6918e4780b9bf83ace3a46048a989d3
a-mute <totocaca@naver.com>
2022-02-20 16:33:54 +0900

    .

---
+++ Flutter/listviewnavigator/android/app/src/main/kotlin/com/example/listviewnavigator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.listviewnavigator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Flutter/listviewnavigator/lib/main.dart
@@ -0,0 +1,28 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+List item = [1,2,3,4];
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        body: Center(
+          child: ListView.builder(
+            itemCount: item.length,
+            itemBuilder: (_, item)),
+        ),
+      ),
+    );
+  }
+}
