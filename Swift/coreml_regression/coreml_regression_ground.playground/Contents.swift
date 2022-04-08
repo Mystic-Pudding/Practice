4eff69c08d02196271f50a1e03e0858c315e108a
a-mystic <totocaca@naver.com>
2022-04-08 22:09:06 +0900

    .

--- Swift/coreml_regression/coreml_regression_ground.playground/Contents.swift
+++ Swift/coreml_regression/coreml_regression_ground.playground/Contents.swift
@@ -6,7 +6,8 @@ let data = try MLDataTable(contentsOf: dataFile)
 print(data.size)
 
 let (trainData, testData) = data.randomSplit(by: 1, seed: 0)
-
-let model = try MLRegressor(trainingData: trainData, targetColumn: "solution")
+print(trainData)
+let params = MLBoostedTreeRegressor.ModelParameters(maxIterations:500)
+let model = try MLBoostedTreeRegressor(trainingData: trainData, targetColumn: "solution", parameters : params)
 let saveModel = MLModelMetadata(author: "mystic", shortDescription: ".", license: ".")
 try model.write(to: URL(fileURLWithPath: "/Users/mystic/Downloads/regression_test.mlmodel"), metadata: saveModel)
