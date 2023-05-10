9368b4c28b4633bc8de9f72f1cfae694f439a54f
a-mystic <totocaca@naver.com>
2023-05-10 13:02:07 +0900

    .

--- Flutter/admob/android/app/src/main/kotlin/com/example/admob/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.admob
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Flutter/listviewnavigator/android/app/src/main/kotlin/com/example/listviewnavigator/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.listviewnavigator
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
