2dd0e9b726ef3b1436175acb27e67fe5230cba34
a-mystic <totocaca@naver.com>
2023-08-29 13:49:52 +0900

    fastapi

--- fastapi tutorial/main.py
+++ fastapi tutorial/main.py
@@ -1,6 +1,30 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 app = FastAPI()
 
-@app.get("/")
-def func():
-    return "fastapi Test"
+class DataInput(BaseModel):
+    name: str
+    ratio: float
+    size: int
+
+class PredictOutput(BaseModel):
+    prob: float
+    prediction: int
+
+@app.post("/pydantic", response_model=PredictOutput)
+def pydanticPost(dataRequest: DataInput):
+    print(dataRequest)
+    return {"prob" : 0.1, "prediction" : 0}
+
+@app.get("/{name}")
+async def root(name: str):
+    return {"message" : "hello"}
+
+@app.get("/home/{name}")
+async def home(name: int):
+    return {"message" : "home"}
+
+@app.post("/")
+def rootPost(msg: str):
+    return {"Hello" : "Post", "msg": msg}
