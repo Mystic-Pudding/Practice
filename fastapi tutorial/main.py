d6953160b22844c9006462eebae1828b773de464
a-mystic <totocaca@naver.com>
2023-10-09 10:17:54 +0900

    .

--- fastapi tutorial/main.py
+++ fastapi tutorial/main.py
@@ -1,15 +1,9 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
 
 app = FastAPI()
 
 # uvicorn main:app --reload
 
-class perfume(BaseModel):
-    title: str
-    description: str
-    thumnailUrl: str
-
 @app.get("/")
 async def hello(name: str):
-    return {"messages" : perfume(title = "perfume name", description = "this perfume is.." ,thumnailUrl = "url")}
+    return {"messages" : "hello"}
